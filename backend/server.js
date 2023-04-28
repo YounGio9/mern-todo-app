96b974c15eb2b5182f0050452693ae7e1ed3d6ba
Giovani <desouzagiovani9@gmail.com>
2023-04-28 22:36:17 +0100

    Restructure frontEnd code

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
+const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8001
 const app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -11,4 +12,4 @@ app.use(bodyParser.json())
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
-} )
+})
